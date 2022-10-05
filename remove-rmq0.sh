512c8589481a5b55879fbc1f54ac0d0a09a79bda
Luke Bakken <luke@bakken.io>
2022-10-05 07:47:51 -0700

    This is the correct sequence of commands

--- remove-rmq0.sh
+++ remove-rmq0.sh
@@ -5,6 +5,15 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+function restart_rmq
+{
+    local -r svc="$1"
+    docker compose stop "$svc"
+    docker compose start "$svc"
+    sleep 1
+    docker compose exec "$svc" rabbitmqctl await_startup
+}
+
 docker compose exec rmq0 rabbitmqctl stop_app
 
 for SVC in rmq1 rmq2
@@ -14,18 +23,11 @@ do
     docker compose exec "$SVC" rabbitmqctl forget_cluster_node rabbit@rmq0
     set -o errexit
 
-    # docker compose exec "$SVC" rabbitmqctl stop_app
-    docker compose stop "$SVC"
-
     # Completely comment out cluster formation with rmq0
     # for the rmq1 and rmq2 nodes configuration
     sed -i.bak -e '/rmq0/s/^/# /' "$SVC.conf"
 
-    # docker compose exec "$SVC" rabbitmqctl start_app
-    docker compose start "$SVC"
-
-    sleep 1
-    docker compose exec "$SVC" rabbitmqctl await_startup
+    restart_rmq "$SVC"
 done
 
 echo ABOUT TO RESET AND START RMQ0, ANY KEY TO CONTINUE
@@ -34,4 +36,4 @@ read
 # Completely comment out cluster formation for rmq0
 sed -i.bak -e '/^cluster_formation/s/^/# /' rmq0.conf
 docker compose exec rmq0 rabbitmqctl reset
-docker compose exec rmq0 rabbitmqctl start_app
+restart_rmq rmq0
